109621e236e45021d4d2783e26904a4fc141df63
pbahrs <pbahrs@us.ibm.com>
2017-10-09 04:01:37 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
 #Define the providers to use
 provider "ibm" {
-  softlayer_username = "${var.slusername}“
-  softlayer_api_key = “${var.slapikey}“
+  softlayer_username = "${var.slusername}"
+  softlayer_api_key = "${var.slapikey}"
 }
 
 #Create file storage
-resource "ibm_storage_file" "fs_single_scaled " {
+resource "ibm_storage_file" "fs_single_scaled" {
   type = "Performance"
-  datacenter = “${var.datacenter}”
+  datacenter = "${var.datacenter}"
   capacity = 20
   iops = 100
   hourly_billing = true
@@ -16,26 +16,25 @@ resource "ibm_storage_file" "fs_single_scaled " {
 #Create multiple VMs
 resource "ibm_compute_vm_instance" "single_scaled_vm_instances" {
   count = "${var.vm_count}"
-  hostname ="${format("single_scaled-%02d", count.index + 1)}"
-  domain =  “${var.domain}”
-  datacenter     = "${var.datacenter}"
+  hostname = "${format("single_scaled-%02d", count.index + 1)}"
+  domain = "${var.domain}"
+  datacenter = "${var.datacenter}"
   file_storage_ids = ["${ibm_storage_file.fs_single_scaled.id}"]
-  network_speed  = “10” 
-  hourly_billing = “true”
+  network_speed  = "10"
+  hourly_billing = "true"
   cores = "1"
   memory = "1024"
   disks = "25, 10"
-  local_disk = “false”
-  private_vlan_id = “${var.privatevlanid}”
-  public_vlan_id = “${var.publicvlanid}”
+  local_disk = false
+  private_vlan_id = "${var.privatevlanid}"
+  public_vlan_id = "${var.publicvlanid}"
 }
-# Define variables 
+#Define variables
 variable slusername {
-  description = ""
+  description = "sl user name"
 }
 variable slapikey {
-  description = ""
-
+  description = "sl api key"
 }
 variable datacenter {
   description = ""
